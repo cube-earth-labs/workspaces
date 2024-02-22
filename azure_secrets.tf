95db03146aca026687887fc5dfc1e434ef564740
Eric Reeves <eric@alluvium.com>
2024-02-21 21:41:35 -0600

    Drop azure_secrets, Add TF Test Module

--- azure_secrets.tf
+++ azure_secrets.tf
@@ -1,104 +1,104 @@
-##########################################################
-# Configuration
-##########################################################
-locals {
-  tfc_labs_role          = "tfc-labs-role"
-  tfc_labs_auth_path     = "jwt-labs"
-  azure_secrets_vcs_repo = "cube-earth-labs/azure_secrets"
-}
+# ##########################################################
+# # Configuration
+# ##########################################################
+# locals {
+#   tfc_labs_role          = "tfc-labs-role"
+#   tfc_labs_auth_path     = "jwt-labs"
+#   azure_secrets_vcs_repo = "cube-earth-labs/azure_secrets"
+# }
 
 
-##########################################################
-# Resources
-##########################################################
-resource "tfe_workspace" "azure_secrets" {
-  name             = "azure_secrets"
-  description      = "Azure Secrets Engine and Key Vault Configuration"
-  organization     = var.tfc_org
-  project_id       = var.labs_project_id
-  auto_apply       = true
-  trigger_patterns = ["/**/*.tf"]
+# ##########################################################
+# # Resources
+# ##########################################################
+# resource "tfe_workspace" "azure_secrets" {
+#   name             = "azure_secrets"
+#   description      = "Azure Secrets Engine and Key Vault Configuration"
+#   organization     = var.tfc_org
+#   project_id       = var.labs_project_id
+#   auto_apply       = true
+#   trigger_patterns = ["/**/*.tf"]
 
-  vcs_repo {
-    identifier     = local.azure_secrets_vcs_repo
-    branch         = "main"
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
-  }
-}
+#   vcs_repo {
+#     identifier     = local.azure_secrets_vcs_repo
+#     branch         = "main"
+#     oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+#   }
+# }
 
-resource "tfe_variable" "azure_enable_vault_provider_auth" {
-  description  = "Enable the Workload Identity integration for Vault."
-  workspace_id = tfe_workspace.azure_secrets.id
-
-  key      = "TFC_VAULT_PROVIDER_AUTH"
-  value    = "true"
-  category = "env"
-}
-
-resource "tfe_variable" "azure_tfc_vault_addr" {
-  description  = "The address of the Vault instance runs will access."
-  workspace_id = tfe_workspace.azure_secrets.id
-
-  key      = "TFC_VAULT_ADDR"
-  value    = var.vault_addr
-  category = "env"
-}
-
-resource "tfe_variable" "azure_tfc_vault_role" {
-  description  = "The Vault role runs will use to authenticate."
-  workspace_id = tfe_workspace.azure_secrets.id
-
-  key      = "TFC_VAULT_RUN_ROLE"
-  value    = local.tfc_labs_role
-  category = "env"
-}
-
-resource "tfe_variable" "azure_tfc_vault_namespace" {
-  description  = "The Vault namespace runs will use to authenticate."
-  workspace_id = tfe_workspace.azure_secrets.id
-
-  key      = "TFC_VAULT_NAMESPACE"
-  value    = var.vault_namespace
-  category = "env"
-}
-
-resource "tfe_variable" "azure_tfc_vault_auth_path" {
-  description  = "The Vault auth path runs will use to authenticate."
-  workspace_id = tfe_workspace.azure_secrets.id
-
-  key      = "TFC_VAULT_AUTH_PATH"
-  value    = local.tfc_labs_auth_path
-  category = "env"
-}
-
-# resource "tfe_variable" "tfc_vault_backed_azure_auth" {
+# resource "tfe_variable" "azure_enable_vault_provider_auth" {
+#   description  = "Enable the Workload Identity integration for Vault."
 #   workspace_id = tfe_workspace.azure_secrets.id
 
-#   key      = "TFC_VAULT_BACKED_AZURE_AUTH"
+#   key      = "TFC_VAULT_PROVIDER_AUTH"
 #   value    = "true"
 #   category = "env"
 # }
 
-# resource "tfe_variable" "tfc_vault_backed_azure_run_vault_role" {
+# resource "tfe_variable" "azure_tfc_vault_addr" {
+#   description  = "The address of the Vault instance runs will access."
 #   workspace_id = tfe_workspace.azure_secrets.id
 
-#   key      = "TFC_VAULT_BACKED_AZURE_RUN_VAULT_ROLE"
-#   value    = "tfc"
+#   key      = "TFC_VAULT_ADDR"
+#   value    = var.vault_addr
 #   category = "env"
 # }
 
-resource "tfe_variable" "azure_subscription_id" {
-  workspace_id = tfe_workspace.azure_secrets.id
+# resource "tfe_variable" "azure_tfc_vault_role" {
+#   description  = "The Vault role runs will use to authenticate."
+#   workspace_id = tfe_workspace.azure_secrets.id
 
-  key      = "azure_subscription_id"
-  value    = var.arm_subscription_id
-  category = "terraform"
-}
+#   key      = "TFC_VAULT_RUN_ROLE"
+#   value    = local.tfc_labs_role
+#   category = "env"
+# }
 
-resource "tfe_variable" "azure_tenant_id" {
-  workspace_id = tfe_workspace.azure_secrets.id
+# resource "tfe_variable" "azure_tfc_vault_namespace" {
+#   description  = "The Vault namespace runs will use to authenticate."
+#   workspace_id = tfe_workspace.azure_secrets.id
 
-  key      = "azure_tenant_id"
-  value    = var.arm_tenant_id
-  category = "terraform"
-}
+#   key      = "TFC_VAULT_NAMESPACE"
+#   value    = var.vault_namespace
+#   category = "env"
+# }
+
+# resource "tfe_variable" "azure_tfc_vault_auth_path" {
+#   description  = "The Vault auth path runs will use to authenticate."
+#   workspace_id = tfe_workspace.azure_secrets.id
+
+#   key      = "TFC_VAULT_AUTH_PATH"
+#   value    = local.tfc_labs_auth_path
+#   category = "env"
+# }
+
+# # resource "tfe_variable" "tfc_vault_backed_azure_auth" {
+# #   workspace_id = tfe_workspace.azure_secrets.id
+
+# #   key      = "TFC_VAULT_BACKED_AZURE_AUTH"
+# #   value    = "true"
+# #   category = "env"
+# # }
+
+# # resource "tfe_variable" "tfc_vault_backed_azure_run_vault_role" {
+# #   workspace_id = tfe_workspace.azure_secrets.id
+
+# #   key      = "TFC_VAULT_BACKED_AZURE_RUN_VAULT_ROLE"
+# #   value    = "tfc"
+# #   category = "env"
+# }
+
+# resource "tfe_variable" "azure_subscription_id" {
+#   workspace_id = tfe_workspace.azure_secrets.id
+
+#   key      = "azure_subscription_id"
+#   value    = var.arm_subscription_id
+#   category = "terraform"
+# }
+
+# resource "tfe_variable" "azure_tenant_id" {
+#   workspace_id = tfe_workspace.azure_secrets.id
+
+#   key      = "azure_tenant_id"
+#   value    = var.arm_tenant_id
+#   category = "terraform"
+# }
---
+++ terraform-aws-s3-website-tests.tf
@@ -0,0 +1,40 @@
+##########################################################
+# Configuration
+##########################################################
+locals {
+  tf_test_vcs_repo    = "cube-earth-labs/terraform-aws-s3-website-tests"
+  tf_test_name        = "terraform-aws-s3-website-tests"
+  tf_test_description = "Terraform Tests - AWS S3 Website"
+}
+
+
+# ##########################################################
+# # Resources
+# ##########################################################
+# resource "tfe_workspace" "this" {
+#   name             = locals.name
+#   description      = locals.description
+#   organization     = var.tfc_org
+#   project_id       = var.labs_project_id
+#   auto_apply       = true
+#   trigger_patterns = ["/**/*.tf"]
+
+#   vcs_repo {
+#     identifier     = local.tf_test_vcs_repo
+#     branch         = "main"
+#     oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+#   }
+# }
+
+resource "tfe_registry_module" "this" {
+  test_config {
+    tests_enabled = true
+  }
+
+  vcs_repo {
+    display_identifier = local.tf_test_vcs_repo
+    identifier         = local.tf_test_vcs_repo
+    oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
+    branch             = "main"
+  }
+}
